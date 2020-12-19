37700264911670767264875c3443c78667ef8d0a
karlic-luka <luka.karlic.math@gmail.com>
2020-12-19 16:29:34 +0100

    Displaying input images as gray

--- test.cpp
+++ test.cpp
@@ -1,25 +1,55 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 // using namespace cv;
+using namespace std;
+
+void showImage(const cv::Mat &image, std::string windowName)
+{
+    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
+    cv::imshow(windowName, image);
+}
+
+void destroyWindows()
+{
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+}
+
+int absoluteDifference(const int &number1, const int &number2)
+{
+    return number1 >= number2 ? (number1 - number2) : (number2 - number1);
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+        printf("usage: DisplayImage.out <Image1_Path> <Image2_Path>\n");
         return -1;
     }
-    cv::Mat image, image2;
-    image = cv::imread(argv[1]);
+    cv::Mat image1, image2, gray1, gray2;
+    image1 = cv::imread(argv[1]);
     image2 = cv::imread(argv[2]);
-    if (!image.data || !image2.data)
+    cv::cvtColor(image1, gray1, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(image2, gray2, cv::COLOR_BGR2GRAY);
+    assert(gray1.channels() == gray2.channels() == 1);
+    
+    if (!image1.data || !image2.data)
     {
         printf("No image data \n");
         return -1;
     }
-    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
-    cv::namedWindow("Display Image2", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Display Image", image);
-    cv::imshow("Display Image2", image2);
-    cv::waitKey(0);
-    return 0;
+    //provjeravam radi li AD 
+    // cout << absoluteDifference(5, 3) << " " << absoluteDifference(3, 5) << endl;
+    cout << image1.size() << endl; //width x height
+    // cv::Mat roi(image1, cv::Range(50, 52), cv::Range(10, 15)); //range of interest
+    // cout << "ROI(numpy" << endl;
+    // cout << cv::format(roi, cv::Formatter::FMT_NUMPY) << endl;
+
+    //prikazi obje slike
+    showImage(gray1, "Image1");
+    showImage(gray2, "Image2");
+    //cout << gray1.channels() << endl;
+    
+    destroyWindows();
 }
