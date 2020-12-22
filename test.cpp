6c8918c1ab0fe6eb2e5a9bf43098371a077604b0
karlic-luka <luka.karlic.math@gmail.com>
2020-12-22 07:38:55 +0100

    Checking coordinate values

--- test.cpp
+++ test.cpp
@@ -79,49 +79,18 @@ int main(int argc, char **argv)
             ADlookUpTable[column][row] = absoluteDifference(row, column);
         }
     }
-    // cout << image1.size() << endl;
-    // cout << ADlookUpTable[5][250] << endl;
-    // cout << ADlookUpTable[255][255] << endl;
-    // cout << ADlookUpTable[0][0] << endl;
 
     //---------------------------------------------------------------------------------
     // cv::Mat roi(image1, cv::Range(50, 52), cv::Range(10, 15)); //range of interest
     // cout << "ROI(numpy" << endl;
     // cout << cv::format(roi, cv::Formatter::FMT_NUMPY) << endl;
 
-    //prikazi obje slike
-    // showImage(image1, "Image1");
-    // showImage(image2, "Image2");
-    // showImage(gray1, "Left");
-    // showImage(gray2, "Right");
-    //cout << gray1.channels() << endl;
-    // cout << image1.size[0] << endl;
-    //inicijalizaciju prvih d_min stupaca OUTPUT mape mogu uciniti preko "view"-a na tu matricu
-    //buduci da se zapravo dijeli header
-    // cv::Mat initialColumns(image1, cv::Range::all(), cv::Range(0, d_min));
-    // cv::Mat initialColumns(d_min, imageHeight, CV_8UC1);
-    // cout << initialColumns.size() << endl;
-    // cv::Mat initialColumns(gray1, cv::Range::all(), cv::Range(0, d_min));
-    // cv::Mat outputMap(imageHeight, imageWidth, CV_8UC1);
-    // cv::Rect roi(0, 0, d_min, imageHeight);
-    // cv::Mat sourceWindow = gray1(roi);
-    // cv::Mat targetWindow = outputMap(roi);
-    // sourceWindow.copyTo(targetWindow);
-    // showImage(outputMap, "output");
-    // cv::copyMakeBorder(initialColumns, outputMap, 0, 0, 0, imageWidth - d_min, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0, 0));
-    // cout << outputMap.size() << endl;
-    // initialColumns = gray1(cv::Range::all(), cv::Range(0, d_min)).clone();
-    // cout << "Initial columns: " << initialColumns.size() << endl;
-    // showImage(initialColumns, "columns");
-    // outputMap(cv::Range::all(), cv::Range(0, d_min)) = gray1(cv::Range::all(), cv::Range(0, d_min));
-    // showImage(gray1, "gray1");
-    // cv::copyMakeBorder(gray1,grayPadded, dis)
-    // cout << "Time to calculate per one disparity hypothese: " << t << endl;
 
     int dimensions[3] = {imageHeight, imageWidth, d_max - d_min}; //ovdje cu spremati rezultate operacija po disparitetima
     cv::Mat results(3, dimensions, CV_32SC1, cv::Scalar(0));
     // cout << results.at<int>(3, 4, 5) << endl;
 
+    int count = 0;
     int disparity = 0;
     uchar *p1, *p2;
     for (int i = 0; i < imageWidth; ++i)
@@ -143,6 +112,7 @@ int main(int argc, char **argv)
                 auto secondIntensity = p2[j - disparity];
                 results.at<int>(i, j, disparity) = ADlookUpTable[(int)firstIntensity][(int)secondIntensity];
             }
+            count++;
         }
     }
     cout << results.at<int>(374, 449, 0) << endl;
@@ -157,6 +127,8 @@ int main(int argc, char **argv)
     // cout << (int)p[3] << endl;
     cout << "gray1(450, 375): " << (int)gray1.ptr<uchar>(374)[449] << endl;
     cout << "gray2(450, 375): " << (int)gray2.ptr<uchar>(374)[449] << endl;
+    cout << "result(450, 375):" << (int)results.at<int>(374, 449, 0) << endl;
+    cout << "count: " << count << " size: " << gray1.rows * gray1.cols << endl;
     showImage(gray1, "gray1");
     showImage(gray2, "gray2");
     destroyWindows();
