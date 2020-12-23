d729ccfb75362a2608ef52b1b684870a65a57210
karlic-luka <luka.karlic.math@gmail.com>
2020-12-23 14:08:55 +0100

    Vector of smart pointer to Mat instead of cube(3d mat object)

--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <chrono>
+#include <vector>
+
 // using namespace cv;
 using namespace std;
 using namespace std::chrono;
@@ -58,7 +60,8 @@ int main(int argc, char **argv)
     int imageHeight = image1.size[1];
     assert(image1.size == image2.size); //kasnije jos dodati malo teksta
     int d_min = 0;
-    int d_max = 40;
+    int d_max = 3;
+    int window = 3;
 
     cv::cvtColor(image1, gray1, cv::COLOR_BGR2GRAY);
     cv::cvtColor(image2, gray2, cv::COLOR_BGR2GRAY);
@@ -91,94 +94,55 @@ int main(int argc, char **argv)
     // cout << cv::format(roi, cv::Formatter::FMT_NUMPY) << endl;
 
     int dimensions[3] = {imageHeight, imageWidth, d_max - d_min}; //ovdje cu spremati rezultate operacija po disparitetima
-    cv::Mat results(3, dimensions, CV_32SC1, cv::Scalar(0));
+    // cv::Mat results(3, dimensions, CV_32SC1, cv::Scalar(0));
     // cout << results.at<int>(3, 4, 5) << endl;
 
     //dio koda koji računa cost za svaki piksel i svaki disparity --> BEZ KUMULATIVNOG TROSKA!
     int count = 0;
-    uchar *p1, *p2;
+    uchar *p1, *p2, *p3;
 
     // Get starting timepoint
+    vector<unique_ptr<cv::Mat>> results;
     start = high_resolution_clock::now();
 
     for (int disparity = d_min; disparity <= d_max; disparity++)
     {
+        unique_ptr<cv::Mat> current(new cv::Mat(imageHeight, imageWidth, CV_8UC1));
+
         for (int i = 0; i < imageWidth; ++i)
         {
             //PAZI: OVO JE REDAK --> Y-KOORDINATA
             p1 = gray1.ptr<uchar>(i);
             p2 = gray2.ptr<uchar>(i);
-            // p3 = dimensions.ptr<uchar>(i);
+            p3 = current->ptr<uchar>(i);
             for (int j = 0; j < imageHeight; ++j)
             {
                 //X KOORDINATA
                 if (j < disparity)
                 {
-                    results.at<int>(i, j, disparity) = p1[j];
+                    p3[j] = p1[j];
                 }
                 else
                 {
                     auto firstIntensity = p1[j];
                     auto secondIntensity = p2[j - disparity];
-                    results.at<int>(i, j, disparity) = ADlookUpTable[(int)firstIntensity][(int)secondIntensity];
+                    p3[j] = ADlookUpTable[(int)firstIntensity][(int)secondIntensity];
                     // results.at<int>(i, j, disparity) = absoluteDifference((int)firstIntensity, (int)secondIntensity);
                 }
                 count++;
             }
         }
+        results.push_back(move(current));
     }
     stop = high_resolution_clock::now();
-
-    // Get duration. Substart timepoints to
-    // get durarion. To cast it to proper unit
-    // use duration cast method
     duration = duration_cast<microseconds>(stop - start);
 
     cout << "Average time taken: look-up-table "
          << duration.count() * 1e-6 / (d_max - d_min) << " seconds" << endl;
     cout << "Total time taken (with init): look-up table " << (duration.count() + tableInit.count()) * 1e-6 << " seconds" << endl;
 
-    // Get starting timepoint
-    start = high_resolution_clock::now();
-
-    for (int disparity = d_min; disparity <= d_max; disparity++)
-    {
-        for (int i = 0; i < imageWidth; ++i)
-        {
-            //PAZI: OVO JE REDAK --> Y-KOORDINATA
-            p1 = gray1.ptr<uchar>(i);
-            p2 = gray2.ptr<uchar>(i);
-            // p3 = dimensions.ptr<uchar>(i);
-            for (int j = 0; j < imageHeight; ++j)
-            {
-                //X KOORDINATA
-                if (j < disparity)
-                {
-                    results.at<int>(i, j, disparity) = p1[j];
-                }
-                else
-                {
-                    auto firstIntensity = p1[j];
-                    auto secondIntensity = p2[j - disparity];
-                    // results.at<int>(i, j, disparity) = ADlookUpTable[(int)firstIntensity][(int)secondIntensity];
-                    results.at<int>(i, j, disparity) = absoluteDifference((int)firstIntensity, (int)secondIntensity);
-                }
-                count++;
-            }
-        }
-    }
-    stop = high_resolution_clock::now();
-
-    // Get duration. Substart timepoints to
-    // get durarion. To cast it to proper unit
-    // use duration cast method
-    duration = duration_cast<microseconds>(stop - start);
-
-    cout << "Average time taken: lambda function "
-         << duration.count() * 1e-6 / (d_max - d_min) << " seconds" << endl;
-    cout << "Total time taken: lambda function " << duration.count() * 1e-6 << " seconds" << endl;
-
-    // cout << results.at<int>(374, 449, 2) << endl;
+    cout << "count: " << count << " size: " << gray1.rows * gray1.cols * (d_max - d_min + 1) << endl;
+    // cout << results.at<int>(374, 449, 2) << endl
     // cout << results.at<int>(3, 4, 0) << endl;
     // cout << test.at<int>(150, 200) << endl;
 
@@ -186,10 +150,19 @@ int main(int argc, char **argv)
     // uchar *p = gray1.ptr<uchar>(2);
     // cout << (int)p[3] << endl;
     // cout << "gray1(450, 375): " << (int)gray1.ptr<uchar>(374)[449] << endl;
-    // cout << "gray2(448, 375): " << (int)gray2.ptr<uchar>(374)[349] << endl;
+    // cout << "gray2(450, 375): " << (int)gray2.ptr<uchar>(374)[449] << endl;
+    // cout << "results(450, 375):" << (int)results.at(0)->ptr<uchar>(374)[449] << endl;
+
+    cout << "gray1(150, 300): " << (int)gray1.ptr<uchar>(150)[300] << endl;
+    cout << "gray2(150, 298): " << (int)gray2.ptr<uchar>(150)[298] << endl;
+    cout << "results(450, 375):" << (int)results.at(2)->ptr<uchar>(150)[300] << endl;
+    // cout << results.size() << endl;
     // cout << "result(450, 375):" << (int)results.at<int>(374, 449, 100) << endl; //(red, stupac, disparity)
 
-    cout << "count: " << count << " size: " << gray1.rows * gray1.cols * (d_max - d_min + 1) << endl;
+    // for(int disparity = d_min; disparity <= d_max; disparity++) {
+
+    // }
+
     // showImage(gray1, "gray1");
     // showImage(gray2, "gray2");
     // destroyWindows();
