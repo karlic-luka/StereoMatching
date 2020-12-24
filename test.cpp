e4f499999f5a4b7822793cea69695bd8f22e8951
karlic-luka <luka.karlic.math@gmail.com>
2020-12-24 01:29:40 +0100

    Strange output disparity map

--- test.cpp
+++ test.cpp
@@ -27,26 +27,6 @@ void destroyWindows()
 auto absoluteDifference = [&](int first, int second) { return first >= second ? (first - second) : (second - first); }; //pripaziti na LAMBDU
 int ADlookUpTable[256][256];                                                                                            //trenutno globalna varijabla, ali bit ce privatna varijabla u objektnoj paradigmi za pripadni matching cost
 
-// cv::Mat &scanImageAndApplyCost(cv::Mat &image1, cv::Mat &image2, cv::Mat &destination, const uchar *const table)
-// {
-//     //accept only char type matrices
-//     CV_Assert(image1.depth() == CV_8U);
-//     //dolazi ce samo grayscale slike
-//     //Since we are reading the image from disk and imread uses the create method, we can simply loop over all pixels using simple pointer arithmetic that does not require a multiplication.
-//     int numberOfRows = image.rows;
-//     int numberOfColumns = image.cols;
-
-//     uchar *p;
-//     for (int i = 0; i < numberOfRows; ++i)
-//     {
-//         p1 = image1.ptr<uchar>(i);
-//         p2 = image2.ptr<uchar>(i);
-//         for (int j = 0; j < numberOfColumns; ++j)
-//         {
-//             destination.at<>(i, j, 0) = ADlookUpTable(p1[j], p2[j]);
-//         }
-//     }
-// }
 int main(int argc, char **argv)
 {
     if (argc != 3)
@@ -60,9 +40,10 @@ int main(int argc, char **argv)
     int columns = image1.cols;
     int rows = image1.rows;
     assert(image1.size == image2.size); //kasnije jos dodati malo teksta
-    int d_min = 0;
-    int d_max = 10;
-    int window = 3;
+    int d_min = 10;
+    int d_max = 150;
+    int radius = 4;
+    int window = 2 * radius + 1;
 
     cv::cvtColor(image1, gray1, cv::COLOR_BGR2GRAY);
     cv::cvtColor(image2, gray2, cv::COLOR_BGR2GRAY);
@@ -73,13 +54,11 @@ int main(int argc, char **argv)
         printf("No image data \n");
         return -1;
     }
-    //provjeravam radi li AD
-    // cout << absoluteDifference(5, 3) << " " << absoluteDifference(3, 5) << endl;
     //----------------------------------LOOKUP TABLICA ZA ABSOLUTE DIFFERENCE------
     auto start = high_resolution_clock::now();
-    for (int row = 0; row < 255; row++)
+    for (int row = 0; row < 256; row++)
     {
-        for (int column = row; column < 255; column++)
+        for (int column = row; column < 256; column++)
         {
             ADlookUpTable[row][column] = absoluteDifference(row, column);
             ADlookUpTable[column][row] = absoluteDifference(row, column);
@@ -94,7 +73,6 @@ int main(int argc, char **argv)
     // cout << "ROI(numpy" << endl;
     // cout << cv::format(roi, cv::Formatter::FMT_NUMPY) << endl;
 
-    // int dimensions[3] = {imageHeight, imageWidth, d_max - d_min}; //ovdje cu spremati rezultate operacija po disparitetima
     // cv::Mat results(3, dimensions, CV_32SC1, cv::Scalar(0));
     // cout << results.at<int>(3, 4, 5) << endl;
 
@@ -108,28 +86,26 @@ int main(int argc, char **argv)
 
     for (int disparity = d_min; disparity <= d_max; disparity++)
     {
-        unique_ptr<cv::Mat> current(new cv::Mat(rows, columns, CV_8UC1));
-
+        unique_ptr<cv::Mat> current(new cv::Mat(rows + 2 * radius, columns + 2 * radius, CV_8UC1, cv::Scalar(0))); //radius=padding
         for (int i = 0; i < rows; ++i)
         {
             //PAZI: OVO JE REDAK --> Y-KOORDINATA
             p1 = gray1.ptr<uchar>(i);
             p2 = gray2.ptr<uchar>(i);
-            p3 = current->ptr<uchar>(i);
+            //+radius jer zelim dodati padding na svaku stranu
+            p3 = current->ptr<uchar>(i + radius);
             for (int j = 0; j < columns; ++j)
             {
                 //X KOORDINATA
                 if (j < disparity)
-                {
-                    p3[j] = p1[j];
-                }
+                    p3[j + radius] = p1[j]; //+radius jer zelim dodati padding na svaku stranu
                 else
                 {
                     auto firstIntensity = p1[j];
                     auto secondIntensity = p2[j - disparity];
-                    p3[j] = ADlookUpTable[(int)firstIntensity][(int)secondIntensity];
+                    //+radius jer zelim dodati padding na svaku stranu
+                    p3[j + radius] = ADlookUpTable[(int)firstIntensity][(int)secondIntensity];
                     // p3[j] = absoluteDifference((int)firstIntensity, (int)secondIntensity);
-                    // results.at<int>(i, j, disparity) = absoluteDifference((int)firstIntensity, (int)secondIntensity);
                 }
                 count++;
             }
@@ -166,30 +142,29 @@ int main(int argc, char **argv)
     int disparityRange = d_max - d_min + 1;
 
     cv::Mat outputMap = cv::Mat::zeros(rows, columns, CV_8UC1);
-    window = 3;
     uchar *pOutput;
     int minIndex, currentCost, leftCost, rightCost;
     start = high_resolution_clock::now();
-    for (int row = window; row < rows - window; row++)
+    for (int row = radius; row < rows + radius; row++)
     {
         p1 = gray1.ptr<uchar>(row);
         p2 = gray2.ptr<uchar>(row);
-        pOutput = outputMap.ptr<uchar>(row);
-        for (int column = window; column < columns - window; column++)
+        pOutput = outputMap.ptr<uchar>(row - radius);
+        for (int column = radius; column < columns + radius - 1; column++)
         {
             int min_cost = window * window * MAX_SINGLE_COST + 1;
-            
+
             for (int i = 0; i < disparityRange; i++)
             {
-                cv::Mat leftColumn(*results.at(i), cv::Range(row, row + window), cv::Range(column, column + 1));
-                cv::Mat rightColumn(*results.at(i), cv::Range(row, row + window), cv::Range(column + window - 1, column + window));
+                cv::Mat leftColumn(*results.at(i), cv::Range(row - radius, row + radius + 1), cv::Range(column - radius, column - radius + 1 + 1)); //+1+1 da se skuzi o cemu se radi
+                cv::Mat rightColumn(*results.at(i), cv::Range(row - radius, row + radius + 1), cv::Range(column + radius, column + radius + 1 + 1));
                 leftCost = (int)cv::sum(leftColumn)[0];
                 rightCost = (int)cv::sum(rightColumn)[0];
-                if (column == 0)
+                if (column == radius) //bilo bi inace 0, al padding
                 {
                     //onda zelim inicijalizirati trenutni prozor i oznaciti lijevi i desni rub --> vec ih imam
                     //lijevi i desni stupac imam vec, samo me zanima sto je izmedu (da ne racunam dvaput)
-                    cv::Mat middle(*results.at(i), cv::Range(row, row + window), cv::Range(1, window - 1));
+                    cv::Mat middle(*results.at(i), cv::Range(row - radius, row + radius + 1), cv::Range(radius + 1, radius + window)); //
                     currentCost = (int)cv::sum(middle)[0] + leftCost + rightCost;
                 }
                 else
@@ -205,18 +180,18 @@ int main(int argc, char **argv)
                 currentCost -= leftCost;
             }
             int disparity = d_min + minIndex;
-            pOutput[column] = disparity;
+            pOutput[column - radius] = disparity;
         }
     }
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     cout << "Total time taken to generate window cost and estimate output map: " << duration.count() * 1e-6 << " seconds" << endl;
 
-    showImage(gray1, "gray1");
-    showImage(gray2, "gray2");
-
-    showImage(outputMap, "output");
     // showImage(gray1, "gray1");
     // showImage(gray2, "gray2");
+    
+    showImage(gray1, "gray1");
+    showImage(gray2, "gray2");
+    showImage(outputMap, "output");
     destroyWindows();
 }
